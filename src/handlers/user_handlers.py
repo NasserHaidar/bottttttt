a7831f0b78be8f92d3f20f17b17bcbf9616ac13d
ScPro <muhamadievbogdan@gmail.com>
2024-11-26 20:31:57 +0500

    development in user_handlers

--- src/handlers/user_handlers.py
+++ src/handlers/user_handlers.py
@@ -2,7 +2,7 @@
 import aiogram
 
 from aiogram import F
-from aiogram.types import Message, CallbackQuery
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup
 from aiogram.filters import StateFilter, Command, CommandStart
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
@@ -10,7 +10,7 @@ from aiogram.fsm.context import FSMContext
 from icecream import ic
 
 from ..keyboards import inline_keyboards
-
+from ..states.user_states import UserStates
 from ..filters import chat_type
 
 #create new Router for handling user messages
@@ -18,14 +18,24 @@ user_router = aiogram.Router()
 user_router.message.filter(chat_type.ChatTypeFilter(["private"]))
 
 #handling /start command
+async def start_handler(message_or_callback, state: FSMContext):
+    await state.set_state(UserStates.main_menu)
+    await message_or_callback.answer(text="Здравствуйте, рады снова вас видеть!", reply_markup=inline_keyboards.main_menu)
+
+@user_router.callback_query(F.data == "back_to_main_menu")
+async def handle_back_to_main_menu(callback: CallbackQuery, state: FSMContext):
+    await start_handler(callback.message, state)
+
 @user_router.message(StateFilter("*"), CommandStart())
-async def start(message: Message):
-    await message.answer(text = "Здравствуйте, рады снова вас видеть!", reply_markup = inline_keyboards.start_keyboard)
+async def handle_start_message(message: Message, state: FSMContext):
+    await start_handler(message, state)
 
+#handling generate avatar
 @user_router.callback_query(F.data == "generate")
-async def start_generate(call: CallbackQuery):
-    await call.message.answer(text = "generate_info")
+async def start_generate(call: CallbackQuery, state: FSMContext):
+    await call.message.answer(text = "Введите текстовый запрос", reply_markup = inline_keyboards.back_to_main_menu)
 
+#handling profile
 @user_router.callback_query(F.data == "profile")
 async def start_generate(call: CallbackQuery):
     await call.message.answer(text = "profile")
--- src/keyboards/inline_keyboards.py
+++ src/keyboards/inline_keyboards.py
@@ -4,12 +4,20 @@ import aiogram.utils.keyboard
 
 from icecream import ic
 
-#start keyboard
-start_keyboard = aiogram.types.InlineKeyboardMarkup(
+#main menu keyboard
+main_menu = aiogram.types.InlineKeyboardMarkup(
     inline_keyboard = [
         [
             aiogram.types.InlineKeyboardButton(text = "Создать аватарку", callback_data = "generate"),
             aiogram.types.InlineKeyboardButton(text = "Профиль", callback_data = "profile"),
+        ]
+    ]
+)
+
+back_to_main_menu = aiogram.types.InlineKeyboardMarkup(
+    inline_keyboard = [
+        [
+            aiogram.types.InlineKeyboardButton(text = "В главное меню", callback_data = "back_to_main_menu"),
         ]
     ]
 )
