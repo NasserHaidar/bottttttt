f1784361cf9f3bc6782ca056ac9cfc458092eddc
peek_A_Bo0o <masser.haidar23@gmail.com>
2024-11-27 21:22:44 +0300

    updates

---
+++ database/engine.py
@@ -0,0 +1,21 @@
+import os
+from database.models.user import Base
+
+from sqlalchemy.ext.asyncio import async_sessionmaker, AsyncSession  , create_async_engine
+from sqlalchemy import create_engine
+
+# Create the SQLAlchemy engine
+engine = create_engine(os.getenv('DB_LITE') , echo=True)
+
+# Create an async session maker
+session_maker = async_sessionmaker(bind=engine, class_=AsyncSession, expire_on_commit=False)
+
+
+async def create_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.create_all)
+
+
+async def drop_db():
+    async with engine.begin() as conn:
+        await conn.run_sync(Base.metadata.drop_all)
--- main.py
+++ main.py
@@ -3,9 +3,14 @@ import aiogram
 import aiogram.filters
 from config import *
 
+from dotenv import find_dotenv , load_dotenv
+load_dotenv(find_dotenv())
 from bot import user_handlers
+from database.engine import create_db, drop_db
 
 from icecream import ic
+
+from database.engine import create_db, drop_db
 
 #constants
 bot = aiogram.Bot(BOT_TOKEN)
@@ -13,11 +18,29 @@ dp = aiogram.Dispatcher() #Dispatcher - main Router
 
 dp.include_router(user_handlers.user_router)
 
+async def on_startup(bot):
+    run_param= False
+    if run_param:
+        await drop_db()
+
+    await create_db()
+
+async def on_shutdown(bot):
+    print('Бот лег')
+
+
 #start polling
 async def main(): # main async fucntion
+    dp.startup.register(on_startup)
+    dp.shutdown.register(on_shutdown)
+
     await bot.delete_webhook(drop_pending_updates = True) #delete webhook
     await bot.send_message(chat_id = scpro_developer_chat_id, text = "started") #sending start debug info 
     await dp.start_polling(bot) # start the bot polling
+
+    # Create the database
+    await create_db()
+    print("Database created successfully!")
 
 if __name__ == "__main__":
     asyncio.run(main())
