908bb72d3a81dc9311c1e0a807b596e1da7d2563
ScPro <muhamadievbogdan@gmail.com>
2024-11-29 22:52:18 +0500

    add balance and photo menu

--- bot/handlers/user_handlers.py
+++ bot/handlers/user_handlers.py
@@ -5,7 +5,7 @@ import asyncio
 import aiogram
 
 from aiogram import F
-from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, BufferedInputFile, FSInputFile
+from aiogram.types import Message, CallbackQuery, InlineKeyboardMarkup, InlineKeyboardButton, BufferedInputFile, FSInputFile
 from aiogram.filters import StateFilter, Command, CommandStart
 from aiogram.fsm.state import StatesGroup, State
 from aiogram.fsm.context import FSMContext
@@ -32,12 +32,8 @@ user_router.message.filter(chat_type.ChatTypeFilter(["private"]))
 AI_requests = AI_Requests(api_key = os.getenv("api_key"))
 
 #------------------------------------------------MAIN MENU-------------------------------------------------------
-async def start_handler(message: Message, state: FSMContext, session: AsyncSession):
-    await state.set_state(UserStates.main_menu)
-    await message.answer_photo(photo = FSInputFile("assets\\empty_image.png"),
-                               caption = f"Здравствуйте, {message.from_user.username}, рады снова вас видеть!",
-                               reply_markup = inline_keyboards.main_menu)
-    
+@user_router.message(StateFilter("*"), CommandStart())
+async def menu_handle_start_command(message: Message, state: FSMContext, session: AsyncSession):
     if not await database.orm_get_user(session, message.from_user.id):
         data = {
             "id": message.from_user.id,
@@ -49,17 +45,20 @@ async def start_handler(message: Message, state: FSMContext, session: AsyncSessi
         }
         await database.orm_add_user(session, data)
 
-@user_router.callback_query(F.data == "back_to_main_menu")
-async def menu_handle_callback(callback: CallbackQuery, state: FSMContext, session: AsyncSession):
-    await start_handler(callback.message, state, session)
+    await state.set_state(UserStates.main_menu)
+    await message.answer_photo(photo = FSInputFile("assets\\empty_image.png"))
+    await message.answer(text = "Здравствуйте, рады снова вас видеть!\nЧтобы начать, выберите один из вариантов работы с ботом", reply_markup = inline_keyboards.main_menu)
 
-@user_router.message(StateFilter("*"), CommandStart())
-async def menu_handle_start_command(message: Message, state: FSMContext, session: AsyncSession):
-    await start_handler(message, state, session)
+@user_router.callback_query(F.data == "back_to_main_menu")
+async def menu_handle_callback(call: CallbackQuery, state: FSMContext, session: AsyncSession):
+    await call.message.delete()
+    await state.set_state(UserStates.main_menu)
+    await call.message.answer(text = "Здравствуйте, рады снова вас видеть!\nЧтобы начать, выберите один из вариантов работы с ботом", reply_markup = inline_keyboards.main_menu)
 
 #------------------------------------------------GENERATING-------------------------------------------------------
 @user_router.callback_query(F.data == "generate")
-async def start_generate(call: CallbackQuery, state: FSMContext):
+async def set_propmt(call: CallbackQuery, state: FSMContext):
+    await call.message.delete()
     await state.set_state(UserStates.prompt)
     await call.message.answer(text = "Введите текстовый запрос", reply_markup = inline_keyboards.back_to_main_menu)
 
@@ -83,8 +82,34 @@ async def generate_handle_callback(call: CallbackQuery, state: FSMContext):
     await generate(call.message, state)
 
 #-------------------------------------------------PROFILE---------------------------------------------------------
+#PROFILE MENU
 @user_router.callback_query(F.data == "profile")
 async def profile_menu(call: CallbackQuery, state: FSMContext , session: AsyncSession):
-    await state.get_state(UserStates.profile_menu)
-    await call.message.answer(text = f"Профиль пользователя: {call.from_user.username}",
+    """Profile menu"""
+    await call.message.delete()
+    await state.set_state(UserStates.profile_menu)
+    user_data = await database.orm_get_user(session, call.from_user.id)
+    await call.message.answer(text = f"Профиль пользователя {user_data.name}:\nID: {user_data.id}",
                               reply_markup = inline_keyboards.profile_menu)
+
+#BALANCE
+@user_router.callback_query(StateFilter(UserStates.profile_menu, UserStates.top_up_balance), F.data == "balance")
+async def balance_menu(call: CallbackQuery, state: FSMContext, session: AsyncSession):
+    """Balance menu"""
+    await state.set_state(UserStates.balance_menu)
+    user_data = await database.orm_get_user(session, call.from_user.id)
+    await call.message.edit_text(text = f"Текущий баланс: {user_data.balance}", reply_markup = inline_keyboards.balance_menu)
+
+@user_router.callback_query(StateFilter(UserStates.balance_menu), F.data == "top_up_balance")
+async def top_up_balance(call: CallbackQuery, state: FSMContext, session: AsyncSession):
+    """Top up the balance, yookassa api"""
+    await call.message.delete()
+    await state.set_state(UserStates.top_up_balance)
+    await call.message.answer(text = "top_up_balance", 
+                              reply_markup = inline_keyboards.back_to_balance_menu)
+
+#photo
+@user_router.callback_query(StateFilter(UserStates.profile_menu), F.data == "photo")
+async def change_photo(call: CallbackQuery, state: FSMContext, session: AsyncSession):
+    """Changing user photo in profile"""
+    await state.set_state(UserStates.send_photo)
