ff50609c977e174cd538fdf5c234bff2798a5f5c
ScPro <muhamadievbogdan@gmail.com>
2024-12-01 01:40:40 +0500

    fix commits

--- database/models/user.py
+++ database/models/user.py
@@ -2,27 +2,21 @@ from email.policy import default
 
 import datetime
 #from pip._internal.utils import datetime
-from sqlalchemy import Column, String, Integer, Boolean, Numeric, DateTime , func
+from sqlalchemy import Column, String, Integer, Boolean, Numeric, DateTime, BLOB, func
 from sqlalchemy.dialects.mysql import DATETIME
 from sqlalchemy.orm import DeclarativeBase, Mapped
 from sqlalchemy.testing.schema import mapped_column
 
-
 class Base(DeclarativeBase):
-    created:Mapped[DATETIME]= mapped_column(DATETIME , default=func.now())
-    updated: Mapped[DATETIME]= mapped_column(DATETIME , default=func.now() , onupdate=func.now())
-
-
+    created:Mapped[DATETIME] = mapped_column(DATETIME , default = func.now())
+    updated: Mapped[DATETIME] = mapped_column(DATETIME , default = func.now() , onupdate = func.now())
 
 class User(Base):
     __tablename__ = 'user'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key=True, autoincrement=True)
-    name: Mapped[str] = mapped_column(String(150), nullable=False)
-    status_subscription: Mapped[bool] = mapped_column(String(50))  # String for subscription status
-    balance: Mapped[float] = mapped_column(Numeric(10, 2))  # Numeric for monetary values
-    image: Mapped[str] = mapped_column(String(150))
-    date: Mapped[datetime.datetime] = mapped_column(DateTime, default=datetime.datetime.utcnow)
-
-# Database URL for SQLite
-DATABASE_URL = "sqlite:///bot_base.db"
+    id: Mapped[int] = mapped_column(Integer, primary_key = True)
+    name: Mapped[str] = mapped_column(String(150), nullable = False)
+    status_subscription: Mapped[bool] = mapped_column(String(50), nullable = True)  # Use String for subscription status
+    balance: Mapped[float] = mapped_column(Numeric(10, 2), nullable = False, default = 0.00)  # Use Numeric for monetary values
+    image: Mapped[bytes] = mapped_column(BLOB(), nullable = True)
+    date: Mapped[datetime.datetime] = mapped_column(DateTime, default = datetime.datetime.now(datetime.timezone.utc))
