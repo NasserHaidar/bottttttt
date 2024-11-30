eb2a10c62515af4f0271ae233466aebb1cb12311
ScPro <muhamadievbogdan@gmail.com>
2024-12-01 01:37:04 +0500

    fix commits

--- bot/states/user_states.py
+++ bot/states/user_states.py
@@ -2,7 +2,21 @@ from aiogram.fsm.state import State, StatesGroup
 from icecream import ic
 
 class UserStates(StatesGroup):
+    #main menu
     main_menu = State()
+
+    #profile_menu
+    profile_menu = State()
+
+    #balance
+    balance_menu = State()
+    top_up_balance = State()
+
+    #set photo
+    photo_menu = State()
+    send_photo = State()
+
+    #generating
     prompt = State()
     model = State()
     style = State()
