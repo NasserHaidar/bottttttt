200952eb72650243ed61fe4c183aa10aa3c9186e
ScPro <muhamadievbogdan@gmail.com>
2024-12-02 22:53:45 +0500

    not finished Leonardo AI integration

--- database/models/user.py
+++ database/models/user.py
@@ -14,9 +14,9 @@ class Base(DeclarativeBase):
 class User(Base):
     __tablename__ = 'user'
 
-    id: Mapped[int] = mapped_column(Integer, primary_key = True)
+    id: Mapped[int] = mapped_column(Integer(), primary_key = True)
     name: Mapped[str] = mapped_column(String(150), nullable = False)
     status_subscription: Mapped[bool] = mapped_column(String(50), nullable = True)  # Use String for subscription status
     balance: Mapped[float] = mapped_column(Numeric(10, 2), nullable = False, default = 0.00)  # Use Numeric for monetary values
-    image: Mapped[bytes] = mapped_column(BLOB(), nullable = True)
+    image: Mapped[bytes] = mapped_column(String(), nullable = True)
     date: Mapped[datetime.datetime] = mapped_column(DateTime, default = datetime.datetime.now(datetime.timezone.utc))
---
+++ test.py
@@ -0,0 +1,24 @@
+from ai import ai_requests
+
+AI_requests = ai_requests.AI_Requests("961472e7-1de4-471f-b744-63e25a7a2a91")
+
+"""with open("assets\\PhotoXzZachem1024x1024.png", "rb") as image_file:
+    image_bytes = image_file.read()
+    success, image_id = AI_requests.upload_image(image_bytes)
+    print(success, image_id)"""
+
+"""image = AI_requests.get_image("c54422db-dbc1-4582-8778-7dc6dd23ce15")
+if image:
+    with open("retrieved_image.jpg", "wb") as image_file:
+        image_file.write(image)
+    print("Image retrieved and saved successfully.")
+else:
+    print("Failed to retrieve image.")"""
+
+image = AI_requests.generate_image(prompt = "cosmos and starts on background, human on Spacesuit ", image_id = "c54422db-dbc1-4582-8778-7dc6dd23ce15")
+"""if image:
+    with open("retrieved_image.jpg", "wb") as image_file:
+        image_file.write(image)
+    print("Image retrieved and saved successfully.")
+else:
+    print("Failed to retrieve image.")"""
